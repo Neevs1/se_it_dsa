41fcded8976c5eb1427563198601e42d5dacbc81
Neevs1 <neevan.redkar@gmail.com>
2024-07-18 14:53:05 +0530

    Merge Sort

---
+++ 18_07/MergeSort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void ip(int [],int);
+void op(int [],int);
+void merge(int [],int);
+
+int main(){
+
+
+    return 0;
+}
+
+void ip(int arr[],int length){
+    for(int i=0;i<length;i++){
+        cout<<"Enter element at "<<i+1<<endl;
+        cin>>arr[i];
+    }
+}
+
+void ip(int arr[],int length){
+    for(int i=0;i<length;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+void merge(int arr[],int){
+    
+}
